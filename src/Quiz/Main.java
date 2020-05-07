75c181bdc884b033e72b1d036070ac951d43f403
Farhad Sherali <farhad.sherali2013@gmail.com>
2020-05-07 21:31:37 +0200

    Test

--- src/Quiz/Main.java
+++ src/Quiz/Main.java
@@ -1,14 +1,32 @@
 package Quiz;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+	
+	Button button;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Farhad's Quiz");
 		
+		button = new Button();
+		button.setText("A");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 800,550);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 }
