3fa840bb3957f2237b8d818b4eeeee3a8b3da88b
Farhad Sherali <farhad.sherali2013@gmail.com>
2020-05-07 22:30:27 +0200

    fail

---
+++ src/Quiz/Controller.java
@@ -0,0 +1,12 @@
+package Quiz;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    void AnswerA(ActionEvent event) {
+
+    }
+
+}
--- src/Quiz/Main.java
+++ src/Quiz/Main.java
@@ -1,9 +1,14 @@
 package Quiz;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -15,18 +20,29 @@ public class Main extends Application{
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) throws Exception{
 		primaryStage.setTitle("Farhad's Quiz");
-		
-		button = new Button();
+		/*button = new Button();
 		button.setText("A");
+		button.setPrefSize(300, 50);
+		button.setTextAlignment(TextAlignment.LEFT);*/
 		
-		StackPane layout = new StackPane();
+		
+		Pane layout = (Pane) FXMLLoader.load(Main.class.getResource("Sample.fxml"));
 		layout.getChildren().add(button);
+		primaryStage.setScene(new Scene(layout));
+		primaryStage.show();
 		
-		Scene scene = new Scene(layout, 800,550);
+		/* Scene scene = new Scene(layout, 1440,720);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		primaryStage.setResizable(false); */
 	}
+
+	/*@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	} */
 
 }
