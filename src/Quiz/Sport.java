bec320929bfc0f9daabc954d475764c667229bb6
Farre4k <farhad.sherali2013@gmail.com>
2020-05-10 20:52:06 +0200

    Update Sport.java

--- src/Quiz/Sport.java
+++ src/Quiz/Sport.java
@@ -5,11 +5,41 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 
 public class Sport implements EventHandler<ActionEvent>{
+	
+	String[] questions = {
+			"Who is the greatest football player of all time?", 
+			"How many world cups have Brazil won?", 
+			"Who has won the most NBA champpionships?" ,
+			"Who has won more grand slam tennis titles?"
+};
+	
+	String[][] alternatives = {
+			{"Cristiano Ronaldo", "Lionel Messi", "Pele", "Diego Maradona"}, 
+			{"2", "4", "1", "5"}, 
+			{"Kobe Bryant", "Michael Jordan", "Lebron James", "Bill Russel"}, 
+			{"Roger Federer", "Rafael Nadal", "novak djokovic", "Pete Sampras"}
+	};
 
+	public Sport() {
+		
+	}
+	
+	public void nextQuestion() {
+	
+	}
+	
+	
 	@Override
 	public void handle(ActionEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	public void answers() {
+		
+	}
+	
+	public void results() {
+		
+	}
 }
