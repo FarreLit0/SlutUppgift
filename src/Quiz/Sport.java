b5daf6e78d49995b73bfaddc090f0017c298bab9
Farre4k <farhad.sherali2013@gmail.com>
2020-05-10 20:22:33 +0200

    Progress

--- src/Quiz/Sport.java
+++ src/Quiz/Sport.java
@@ -1,5 +1,15 @@
 package Quiz;
 
-public class Sport {
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class Sport implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
